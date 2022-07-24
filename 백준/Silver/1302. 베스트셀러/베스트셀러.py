1d7910e93427a69541f5332961aa4cf0c0181fcb
twowinn <105332962+LSC18@users.noreply.github.com>
2022-07-24 20:23:31 +0900

    [Silver IV] Title: 베스트셀러, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -1,12 +1,12 @@
 N = int(input())
 books = {}
+res = []
 for _ in range(N):
     book = input()
     if book not in books:
         books[book] = 1
     else:
         books[book] += 1
-res = []
 ans = max(books.values())
 for j in books:
     if ans == books[j]:

2485350182f4cf7ee7314ce68e4c35765666ed40
twowinn <105332962+LSC18@users.noreply.github.com>
2022-07-24 19:53:43 +0900

    Update 듣보잡.py

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -13,3 +13,24 @@ result = sorted(list(result))
 print(len(result))
 for _ in result:
     print(_)
+    
+
+# 밑에 코드 == 때려 넣는 삭으로 짠 코드인데 시간초과 뜸. 처음 짠게 잘 짠듯.
+
+# n,m = map(int,input().split())
+# ownli = []
+# newli = []
+# result = []
+# for i in range(n):
+#     ownli.append(input())
+# for j in range(m):
+#     ownli.append(input())
+# for v in ownli:
+#     if v in newli:
+#         result.append(v)
+#     else:
+#         newli.append(v)
+# result = sorted(result)
+# print(len(result))
+# for _ in result:
+#     print(_)

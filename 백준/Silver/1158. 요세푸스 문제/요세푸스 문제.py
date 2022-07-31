08a4418b7c1bb562b8535d2a057b46d31a703ffe
twowinn <105332962+LSC18@users.noreply.github.com>
2022-07-31 19:41:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 96 ms, Memory: 32376 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,20 +1,8 @@
-# from collections import deque
-# n,m = map(int,input().split())
-# lst = deque(range(1,n+1))
-# ans = []
-# while len(lst) >= m:
-#     lst.rotate(-m+1)
-#     ans.append(str(lst.popleft()))
-# for i in lst:
-#     ans.append(str(i))
-# print('<'+', '.join(ans)+'>')
-
 from collections import deque
 n,m = map(int,input().split())
 lst = deque(range(1,n+1))
 ans = []
 while len(lst) > 0:
-    for _ in range(m-1):
-        lst.append(lst.popleft())
+    lst.rotate(-m+1)
     ans.append(str(lst.popleft()))
 print('<'+', '.join(ans)+'>')
